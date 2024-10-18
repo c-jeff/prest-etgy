d7b2307f34b10d6645147d80b61f323a990c189c
c-jeff <powerjspxr@gmail.com>
2024-10-17 23:49:44 -0500

    initializr

---
+++ src/main/java/com/rpost/jsilva/JsilvaApplication.java
@@ -0,0 +1,13 @@
+package com.rpost.jsilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsilvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsilvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpost/jsilva/JsilvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpost.jsilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsilvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
